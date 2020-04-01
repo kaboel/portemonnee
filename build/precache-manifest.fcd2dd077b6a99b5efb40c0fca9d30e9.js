d9a2ae6be56ae706a70da6e8d812d13f5b4eefe2
Faiq Allam <faiq.kaboel@gmail.com>
2020-04-02 06:27:37 +0700

    add build

---
+++ build/precache-manifest.fcd2dd077b6a99b5efb40c0fca9d30e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d3c7bd7b00947d8e261a88bc40eef43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1406579af982852fa3ef",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "77f0a66a8e084be8f2b6",
+    "url": "/static/js/2.4ac717a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4ac717a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1406579af982852fa3ef",
+    "url": "/static/js/main.dce5917d.chunk.js"
+  },
+  {
+    "revision": "0aff61395edd867069be",
+    "url": "/static/js/runtime-main.2acb3552.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
